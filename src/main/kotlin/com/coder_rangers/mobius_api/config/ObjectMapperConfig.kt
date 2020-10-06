433e817eb1bacd1c00328f855586befd43c4330a
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2020-10-06 00:47:20 -0300

    Adding object mapper configuration

---
+++ src/main/kotlin/com/coder_rangers/mobius_api/config/ObjectMapperConfig.kt
@@ -0,0 +1,30 @@
+package com.coder_rangers.mobius_api.config
+
+import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
+import com.fasterxml.jackson.databind.DeserializationFeature.READ_ENUMS_USING_TO_STRING
+import com.fasterxml.jackson.databind.MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.LOWER_CAMEL_CASE
+import com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
+import com.fasterxml.jackson.databind.SerializationFeature.WRITE_ENUMS_USING_TO_STRING
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import java.util.TimeZone
+
+@Configuration
+class ObjectMapperConfig {
+    @Bean
+    @Primary
+    fun snakeCase(): ObjectMapper = jacksonObjectMapper()
+        .registerModule(JavaTimeModule())
+        .enable(WRITE_ENUMS_USING_TO_STRING)
+        .enable(READ_ENUMS_USING_TO_STRING)
+        .enable(ACCEPT_CASE_INSENSITIVE_ENUMS)
+        .disable(WRITE_DATES_AS_TIMESTAMPS)
+        .disable(FAIL_ON_UNKNOWN_PROPERTIES)
+        .setTimeZone(TimeZone.getTimeZone("UTC"))
+        .setPropertyNamingStrategy(LOWER_CAMEL_CASE)
+}
