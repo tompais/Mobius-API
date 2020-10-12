652d435199ae79678b3688b7f3561b13b7ee1b5b
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2020-10-12 16:47:10 -0300

    Implementing patient dao to security service for signing in

--- src/main/kotlin/com/coder_rangers/mobius_api/services/implementations/SecurityService.kt
+++ src/main/kotlin/com/coder_rangers/mobius_api/services/implementations/SecurityService.kt
@@ -1,13 +1,26 @@
 package com.coder_rangers.mobius_api.services.implementations
 
+import com.coder_rangers.mobius_api.dao.interfaces.IPatientDAO
+import com.coder_rangers.mobius_api.error.exceptions.PatientNotFoundException
+import com.coder_rangers.mobius_api.requests.SignInRequest
 import com.coder_rangers.mobius_api.requests.SignUpRequest
 import com.coder_rangers.mobius_api.services.interfaces.ISecurityService
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class SecurityService : ISecurityService {
+@Transactional
+class SecurityService @Autowired constructor(
+    private val patientDAO: IPatientDAO
+) : ISecurityService {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun signUp(signUpRequest: SignUpRequest) = logger.info(signUpRequest.toString())
+
+    override fun signIn(signInRequest: SignInRequest) {
+        patientDAO.findActivePatientByEmailAndPassword("fulanito@gmail.com", "lala1234")
+            ?: throw PatientNotFoundException()
+    }
 }
