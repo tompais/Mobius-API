80a8e1bd77dea81a1c50dd77764d8704add5f6f0
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2020-10-12 16:43:27 -0300

    Adding patient dao

---
+++ src/main/kotlin/com/coder_rangers/mobius_api/dao/implementations/PatientDAO.kt
@@ -0,0 +1,15 @@
+package com.coder_rangers.mobius_api.dao.implementations
+
+import com.coder_rangers.mobius_api.dao.interfaces.IPatientDAO
+import com.coder_rangers.mobius_api.database.repositories.IPatientRepository
+import com.coder_rangers.mobius_api.models.Patient
+import com.coder_rangers.mobius_api.models.User.Status.ACTIVE
+import org.springframework.stereotype.Repository
+
+@Repository
+class PatientDAO(
+    private val patientRepository: IPatientRepository
+) : IPatientDAO {
+    override fun findActivePatientByEmailAndPassword(email: String, password: String): Patient? =
+        patientRepository.findByEmailAndPasswordAndStatus(email, password, ACTIVE)
+}
