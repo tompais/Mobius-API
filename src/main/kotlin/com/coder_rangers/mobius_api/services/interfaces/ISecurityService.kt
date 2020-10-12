54354e400e57604dec58d0a0c94a334d95fd7440
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2020-10-12 16:47:18 -0300

    Adding sign in method

--- src/main/kotlin/com/coder_rangers/mobius_api/services/interfaces/ISecurityService.kt
+++ src/main/kotlin/com/coder_rangers/mobius_api/services/interfaces/ISecurityService.kt
@@ -1,7 +1,9 @@
 package com.coder_rangers.mobius_api.services.interfaces
 
+import com.coder_rangers.mobius_api.requests.SignInRequest
 import com.coder_rangers.mobius_api.requests.SignUpRequest
 
 interface ISecurityService {
     fun signUp(signUpRequest: SignUpRequest)
+    fun signIn(signInRequest: SignInRequest)
 }
