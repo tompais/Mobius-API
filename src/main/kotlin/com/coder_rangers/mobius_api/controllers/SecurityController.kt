2d9ee484b242d10df1aa3577a74e9be217fc254d
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2020-10-13 22:17:14 -0300

    Adding cross origin

--- src/main/kotlin/com/coder_rangers/mobius_api/controllers/SecurityController.kt
+++ src/main/kotlin/com/coder_rangers/mobius_api/controllers/SecurityController.kt
@@ -15,9 +15,11 @@ import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.OK
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod.POST
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
@@ -35,6 +37,7 @@ class SecurityController @Autowired constructor(
             ApiResponse(responseCode = "400", description = "The sign up information that was provided is wrong.")
         ]
     )
+    @CrossOrigin(methods = [POST])
     @PostMapping("/signup")
     @ResponseStatus(CREATED)
     fun signUp(@RequestBody @Valid signUpRequest: SignUpRequest) = securityService.signUp(signUpRequest)
@@ -60,6 +63,7 @@ class SecurityController @Autowired constructor(
             ApiResponse(responseCode = "404", description = "The patient was not found. Check your email and password.")
         ]
     )
+    @CrossOrigin(methods = [POST])
     @PostMapping("/signin")
     @ResponseStatus(OK)
     fun singIn(@RequestBody @Valid signInRequest: SignInRequest): SignInResponse = securityService.signIn(signInRequest)
