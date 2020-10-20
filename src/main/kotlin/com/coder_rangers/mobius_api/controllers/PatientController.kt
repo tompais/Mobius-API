2f58d4b56bc30b795eb1938eb6fbced95b3e826a
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2020-10-19 23:45:39 -0300

    Documenting endpoint

--- src/main/kotlin/com/coder_rangers/mobius_api/controllers/PatientController.kt
+++ src/main/kotlin/com/coder_rangers/mobius_api/controllers/PatientController.kt
@@ -2,12 +2,20 @@ package com.coder_rangers.mobius_api.controllers
 
 import com.coder_rangers.mobius_api.models.Game
 import com.coder_rangers.mobius_api.services.interfaces.IPatientService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus.OK
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.constraints.Positive
 
@@ -17,6 +25,34 @@ import javax.validation.constraints.Positive
 class PatientController @Autowired constructor(
     private val patientService: IPatientService
 ) {
+    @Operation(summary = "Endpoint to get the mental test game that user has to do.")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "There's a game that the user should do to finish the test.",
+                content = [
+                    Content(
+                        mediaType = APPLICATION_JSON_VALUE,
+                        array = ArraySchema(
+                            schema = Schema(
+                                implementation = Game::class
+                            )
+                        )
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Patient not found exception"
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "The patient may have finished the test."
+            )
+        ]
+    )
     @GetMapping("/{id}/mental-test/game")
+    @ResponseStatus(OK)
     fun getMentalTestGame(@Positive @PathVariable("id") id: Long): Game = patientService.getMentalTestGame(id)
 }
