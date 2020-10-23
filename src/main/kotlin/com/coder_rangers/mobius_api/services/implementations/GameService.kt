2fdacef56caa359bea2648186f0b22a5610d2632
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2020-10-23 20:22:00 -0300

    Reverting get specific game category

--- src/main/kotlin/com/coder_rangers/mobius_api/services/implementations/GameService.kt
+++ src/main/kotlin/com/coder_rangers/mobius_api/services/implementations/GameService.kt
@@ -4,10 +4,6 @@ import com.coder_rangers.mobius_api.dao.interfaces.IGameDAO
 import com.coder_rangers.mobius_api.error.exceptions.GameNotFoundException
 import com.coder_rangers.mobius_api.models.Game
 import com.coder_rangers.mobius_api.models.Game.Category
-import com.coder_rangers.mobius_api.models.Game.Category.ORIENTATION
-import com.coder_rangers.mobius_api.models.Input
-import com.coder_rangers.mobius_api.models.Input.Type.TEXT
-import com.coder_rangers.mobius_api.models.Task
 import com.coder_rangers.mobius_api.services.interfaces.IGameService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -27,27 +23,7 @@ class GameService @Autowired constructor(
         return getGameById(randomId)
     }
 
-    override fun getSpecificGameByCategory(category: Category): Game {
-        return when (category) {
-            ORIENTATION -> gameDAO.findGameByCategory(category)
-            else -> Game(
-                0,
-                "Juego de memoria",
-                "",
-                ORIENTATION,
-                listOf(
-                    Task(
-                        0, null, "¿Puede recordar las palabras que le dije antes?",
-                        listOf(
-                            Input(0, TEXT),
-                            Input(0, TEXT),
-                            Input(0, TEXT)
-                        )
-                    )
-                )
-            )
-        }
-    }
+    override fun getSpecificGameByCategory(category: Category): Game = gameDAO.findGameByCategory(category)
 
     override fun getGameById(id: Long): Game = gameDAO.findGameById(id) ?: throw GameNotFoundException(id)
 }
