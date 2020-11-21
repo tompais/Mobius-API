f9076299760d8c68ad092f24655eeec3ecb9c43e
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2020-11-21 15:01:17 -0300

    Checking illegal extension

--- src/main/kotlin/com/coder_rangers/mobius_api/services/implementations/ImageService.kt
+++ src/main/kotlin/com/coder_rangers/mobius_api/services/implementations/ImageService.kt
@@ -1,8 +1,10 @@
 package com.coder_rangers.mobius_api.services.implementations
 
+import com.coder_rangers.mobius_api.error.exceptions.IllegalImageExtensionException
 import com.coder_rangers.mobius_api.responses.UploadImageResponse
 import com.coder_rangers.mobius_api.services.interfaces.IImageService
 import org.apache.commons.io.FileUtils
+import org.apache.commons.io.FilenameUtils
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
@@ -11,6 +13,10 @@ import java.util.UUID
 @Service
 class ImageService : IImageService {
     override fun saveImage(imageFile: MultipartFile): UploadImageResponse {
+        if (!isPng(imageFile)) {
+            throw IllegalImageExtensionException()
+        }
+
         val fileName = buildFileName()
         val filePath = buildFilePath(fileName)
 
@@ -18,6 +24,9 @@ class ImageService : IImageService {
 
         return UploadImageResponse(fileName)
     }
+
+    private fun isPng(imageFile: MultipartFile) =
+        FilenameUtils.getExtension(imageFile.originalFilename) == "png"
 
     private fun buildFilePath(fileName: String): String = "src/main/resources/drawings/$fileName"
 
